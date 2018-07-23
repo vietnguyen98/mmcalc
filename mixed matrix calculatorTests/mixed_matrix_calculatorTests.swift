3fc5a4e498b156d3fe13449cd7a956b86fa42f43
Viet Nguyen <vietnguyen@viets-macbook-pro.local>
2018-07-23 15:46:10 -0700

    Initial Commit

---
+++ mixed matrix calculatorTests/mixed_matrix_calculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  mixed_matrix_calculatorTests.swift
+//  mixed matrix calculatorTests
+//
+//  Created by Viet Nguyen on 7/23/18.
+//  Copyright © 2018 Viet Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import mixed_matrix_calculator
+
+class mixed_matrix_calculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
